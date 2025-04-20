ee7f7e09a591aee464fd716bf36054f8d7987f2f
Rikthi Pranadhik Mondal <rikpm8141522@gmail.com>
2025-04-20 04:04:58 -0600

    Item and Inventory Entities created

--- src/main/java/dev/rikthipranadhik/storemanagementsystembackend/entity/Inventory/Inventory.java
+++ src/main/java/dev/rikthipranadhik/storemanagementsystembackend/entity/Inventory/Inventory.java
@@ -1,22 +1,43 @@
 package dev.rikthipranadhik.storemanagementsystembackend.entity.Inventory;
 
+import dev.rikthipranadhik.storemanagementsystembackend.entity.store.Store;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name="Inventory")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name= "inventory_id", nullable = false)
+    @Column(name= "inventory_id", nullable = false, unique = true)
     private Long id;
 
     @Column(name="category")
     private String Category;
+
+    @Column(name="current_stock_level")
+    private Long currentStockLevel;
+
+    @Column(name="minimum_stock_level")
+    private Long minimumStockLevel;
+
+    @Column(name="maximum_stock_level")
+    private Long maximumStockLevel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name= "store_id", nullable=false)
+    private Store store;
 
 
 }
--- src/main/java/dev/rikthipranadhik/storemanagementsystembackend/entity/Inventory/Item.java
+++ src/main/java/dev/rikthipranadhik/storemanagementsystembackend/entity/Inventory/Item.java
@@ -1,4 +1,50 @@
 package dev.rikthipranadhik.storemanagementsystembackend.entity.Inventory;
 
+import dev.rikthipranadhik.storemanagementsystembackend.entity.store.Store;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="item_id", nullable=false, unique=true)
+    private Long id;
+
+    @Column(name= "item_name", nullable=false)
+    private String name;
+
+    @Column(name= "manufacture_date")
+    private LocalDate manufactureDate;
+
+    @Column(name= "expiration_date")
+    private LocalDate expirationDate;
+
+    @Column(name= "price")
+    private float price;
+
+    @Column(name= "discount_percentage")
+    private float discountPercentage;
+
+    @Column(name="quantity")
+    private Long quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    @JoinColumn(name="inventory_id")
+    private Inventory inventory;
+
+
 }
