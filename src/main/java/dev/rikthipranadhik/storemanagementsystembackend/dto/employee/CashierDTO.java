6007a97b1570a64d2213b573af196d442e3d56ac
Rikthi Pranadhik Mondal <rikpm8141522@gmail.com>
2025-04-22 05:14:38 -0600

    production changes made

--- src/main/java/dev/rikthipranadhik/storemanagementsystembackend/dto/employee/CashierDTO.java
+++
@@ -1,4 +0,0 @@
-package dev.rikthipranadhik.storemanagementsystembackend.dto.employee;
-
-public record CashierDTO(EmployeeDTO employee) {
-}
--- src/main/java/dev/rikthipranadhik/storemanagementsystembackend/dto/employee/ClerkDTO.java
+++
@@ -1,4 +0,0 @@
-package dev.rikthipranadhik.storemanagementsystembackend.dto.employee;
-
-public record ClerkDTO(EmployeeDTO employee) {
-}
